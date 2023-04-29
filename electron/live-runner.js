0082bbb64d2871dd8d050ef49bd7461cc3cc1024
KosukeIsogai <kouiso@ritmo.co.jp>
2023-04-29 12:46:02 +0900

    fixした

--- electron/live-runner.js
+++ electron/live-runner.js
@@ -1,44 +1,44 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const cp = require('child_process');
-const chokidar = require('chokidar');
-const electron = require('electron');
+const cp = require('child_process')
+const chokidar = require('chokidar')
+const electron = require('electron')
 
-let child = null;
-const npmCmd = process.platform === 'win32' ? 'npm.cmd' : 'npm';
+let child = null
+const npmCmd = process.platform === 'win32' ? 'npm.cmd' : 'npm'
 const reloadWatcher = {
   debouncer: null,
   ready: false,
   watcher: null,
   restarting: false,
-};
+}
 
 ///*
 function runBuild() {
   return new Promise((resolve, _reject) => {
-    let tempChild = cp.spawn(npmCmd, ['run', 'build']);
+    let tempChild = cp.spawn(npmCmd, ['run', 'build'])
     tempChild.once('exit', () => {
-      resolve();
-    });
-    tempChild.stdout.pipe(process.stdout);
-  });
+      resolve()
+    })
+    tempChild.stdout.pipe(process.stdout)
+  })
 }
 //*/
 
 async function spawnElectron() {
   if (child !== null) {
-    child.stdin.pause();
-    child.kill();
-    child = null;
-    await runBuild();
+    child.stdin.pause()
+    child.kill()
+    child = null
+    await runBuild()
   }
-  child = cp.spawn(electron, ['--inspect=5858', './']);
+  child = cp.spawn(electron, ['--inspect=5858', './'])
   child.on('exit', () => {
     if (!reloadWatcher.restarting) {
-      process.exit(0);
+      process.exit(0)
     }
-  });
-  child.stdout.pipe(process.stdout);
+  })
+  child.stdout.pipe(process.stdout)
 }
 
 function setupReloadWatcher() {
@@ -48,28 +48,28 @@ function setupReloadWatcher() {
       persistent: true,
     })
     .on('ready', () => {
-      reloadWatcher.ready = true;
+      reloadWatcher.ready = true
     })
     .on('all', (_event, _path) => {
       if (reloadWatcher.ready) {
-        clearTimeout(reloadWatcher.debouncer);
+        clearTimeout(reloadWatcher.debouncer)
         reloadWatcher.debouncer = setTimeout(async () => {
-          console.log('Restarting');
-          reloadWatcher.restarting = true;
-          await spawnElectron();
-          reloadWatcher.restarting = false;
-          reloadWatcher.ready = false;
-          clearTimeout(reloadWatcher.debouncer);
-          reloadWatcher.debouncer = null;
-          reloadWatcher.watcher = null;
-          setupReloadWatcher();
-        }, 500);
+          console.log('Restarting')
+          reloadWatcher.restarting = true
+          await spawnElectron()
+          reloadWatcher.restarting = false
+          reloadWatcher.ready = false
+          clearTimeout(reloadWatcher.debouncer)
+          reloadWatcher.debouncer = null
+          reloadWatcher.watcher = null
+          setupReloadWatcher()
+        }, 500)
       }
-    });
+    })
 }
 
-(async () => {
-  await runBuild();
-  await spawnElectron();
-  setupReloadWatcher();
-})();
+;(async () => {
+  await runBuild()
+  await spawnElectron()
+  setupReloadWatcher()
+})()
