87bc6e07f3cca06f4b35d0547f4fbb9a43e6f37a
KosukeIsogai <kouiso@ritmo.co.jp>
2023-04-29 20:00:44 +0900

    コミット

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,15 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
+  "auth": {
     "horsemanager": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
   }
 }
