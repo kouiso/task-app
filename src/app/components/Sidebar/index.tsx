c57c1dd5dd7223ca3eaa7f51d38b2d3a3f431d3d
Kawakatsu <samurai.blue6111@gmail.com>
2024-11-10 14:20:20 +0900

    pr修正

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -9,50 +9,8 @@ import HamburgerIcon from '../Hamburger/index.icon';
 import SidebarItem from '../SidebarItem';
 import ToggleTheme from '../ToggleTheme';
 
-import AccountBoxIcon from './account-box.icon';
-import AddToPhotosIcon from './add-to-photo.icon';
-import BarChartIcon from './bar-chart.icon';
-import EventNoteIcon from './event-note.icon';
+import menus from './constants';
 import styles from './style.module.scss';
-
-const menus = [
-  {
-    id: 'account',
-    title: 'アカウント',
-    icon: <AccountBoxIcon />,
-    subMenus: [
-      { id: 'sub1', href: '/__TBD__', label: 'サブメニュー1' },
-      { id: 'sub2', href: '/__TBD__', label: 'サブメニュー2' },
-    ],
-  },
-  {
-    id: 'record',
-    title: '記録',
-    icon: <BarChartIcon />,
-    subMenus: [
-      { id: 'sub1', href: '/__TBD__', label: 'サブメニュー1' },
-      { id: 'sub2', href: '/__TBD__', label: 'サブメニュー2' },
-      { id: 'sub3', href: '/__TBD__', label: 'サブメニュー3' },
-      { id: 'sub4', href: '/__TBD__', label: 'サブメニュー4' },
-    ],
-  },
-  {
-    id: 'register',
-    title: '登録',
-    icon: <AddToPhotosIcon />,
-    subMenus: [{ id: 'sub1', href: '/__TBD__', label: 'サブメニュー1' }],
-  },
-  {
-    id: 'other',
-    title: 'その他',
-    icon: <EventNoteIcon />,
-    subMenus: [
-      { id: 'sub1', href: '/__TBD__', label: 'サブメニュー1' },
-      { id: 'sub2', href: '/__TBD__', label: 'サブメニュー2' },
-      { id: 'sub3', href: '/__TBD__', label: 'サブメニュー3' },
-    ],
-  },
-] as const;
 
 const Sidebar = () => {
   const [isActive, setIsActive] = useState<boolean>(false);
